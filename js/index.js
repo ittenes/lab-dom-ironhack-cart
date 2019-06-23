843c56cd3ec7c2f15a6f53bc0ae7d66b88d4de1c
Antonio Rivera <antonioriver@gmail.com>
2019-06-23 14:44:37 +0200

    finish on my way

--- js/index.js
+++ js/index.js
@@ -1,9 +1,15 @@
 function deleteItem(event) {
+  var parent = event.currentTarget.parentNode.parentNode
+
+  parent.parentNode.removeChild(parent)
 
 }
 
+
+
 function getPriceByProduct(itemNode) {
-  var precioId = document.getElementById(itemNode).textContent;;
+  var precioId = itemNode.getElementsByClassName("precio")[0].textContent;
+
   return precioId
 }
 
@@ -12,11 +18,38 @@ function updatePriceByProduct(productPrice, index) {
 }
 
 function getTotalPrice() {
+  const inputQTs = document.getElementsByClassName("input")
+  console.log("TCL: getTotalPrice -> inputQTs", inputQTs)
 
-  const inputQT = document.querySelector("input")
-  const precioValue = getPriceByProduct("precio");
+  for (let i = 0; i < inputQTs.length; i++) {
+    inputQT = inputQTs[i].value
+    console.log("TCL: getTotalPrice -> inputQT", inputQT)
+    inputQTNode = inputQTs[i].parentNode.parentNode;
+    console.log("TCL: getTotalPrice -> inputQTNode", inputQTNode)
+    precioValue = Number(getPriceByProduct(inputQTNode));
+    console.log("TCL: getTotalPrice -> precioValue", precioValue)
+    inputQTNode.getElementsByClassName('total')[0].innerHTML = (Number(inputQT) * Number(precioValue));
+  }
 
-  document.getElementById("total").innerHTML = (Number(inputQT.value) * Number(precioValue));
+  // inputQTs.forEach(function (element) {
+  //   inputQT = element.value
+  //   inputQTNode = element.parentNode.parentNode;
+  //   precioValue = Number(getPriceByProduct(inputQTNode));
+  //   inputQTNode.getElementsByClassName('total')[0].innerHTML = (Number(inputQT) * Number(precioValue));
+  // })
+
+  calculateTotalPrice();
+}
+
+function calculateTotalPrice() {
+  let arrElements = document.getElementsByClassName("total");
+  var sumaValue = 0
+
+  for (let i = 0; i < arrElements.length; i++) {
+    sumaValue += Number(arrElements[i].textContent)
+  }
+
+  document.getElementById('total-value').innerHTML = Number(sumaValue);
 }
 
 function createQuantityInput() {
@@ -24,7 +57,10 @@ function createQuantityInput() {
 }
 
 function createDeleteButton() {
-
+  var deleteButtons = document.getElementsByClassName('btn-delete');
+  for (var i = 0; i < deleteButtons.length; i++) {
+    deleteButtons[i].onclick = deleteItem;
+  }
 }
 
 function createQuantityNode() {
@@ -41,6 +77,32 @@ function createNewItemRow(itemName, itemUnitPrice) {
 
 function createNewItem() {
 
+  let itemName = document.getElementById("new-name").value;
+  let itemUnitPrice = document.getElementById("new-cost").value;
+
+  let nodeParent = document.getElementById("all-wrapper")
+  let div = document.createElement("div")
+  div.className = "wrapper"
+  div.innerHTML = `
+    <div>
+      <span>${itemName}</span>
+    </div>
+    <div>
+      <span class="euro precio">${itemUnitPrice}</span>
+    </div>
+    <div class="quantity">
+      QT: <input class="input" type="text" name="fname" />
+    </div>
+    <div>
+      <span class="euro total">0.00</span>
+    </div>
+    <div>
+      <button class="btn btn-delete ">delete</button>
+    </div>
+  `;
+  nodeParent.appendChild(div)
+  createDeleteButton();
+  createNewItemRow(itemName, itemUnitPrice);
 }
 
 window.onload = function () {
@@ -51,7 +113,9 @@ window.onload = function () {
   calculatePriceButton.onclick = getTotalPrice;
   createItemButton.onclick = createNewItem;
 
+
   for (var i = 0; i < deleteButtons.length; i++) {
     deleteButtons[i].onclick = deleteItem;
   }
+
 };
